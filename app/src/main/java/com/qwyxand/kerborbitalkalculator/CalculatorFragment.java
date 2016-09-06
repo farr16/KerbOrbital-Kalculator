65fad8895eb46a75485c95a2fec043ddeb7ed156
Matthew Farr <msamfarr@outlook.com>
2016-09-05 23:03:12 -0700

    Adds array adapter to populate the spinners in the calculator fragment with choices

--- app/src/main/java/com/qwyxand/kerborbitalkalculator/CalculatorFragment.java
+++ app/src/main/java/com/qwyxand/kerborbitalkalculator/CalculatorFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /** CalculatorFragment
  * Created by Matthew on 9/2/2016.
@@ -15,6 +17,21 @@ import android.view.ViewGroup;
 public class CalculatorFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calculator, container, false);
+        View calcView = inflater.inflate(R.layout.fragment_calculator, container, false);
+
+        // Initialize array of planets to be used in origin and destination spinners
+        String[] planets = {"Moho", "Eve", "Kerbin", "Duna", "Dres", "Jool", "Eeloo"};
+
+        // Obtain references to layout spinners in code
+        Spinner origin = (Spinner) calcView.findViewById(R.id.originSelector);
+        Spinner destination = (Spinner) calcView.findViewById(R.id.destinationSelector);
+
+        // Set array adapters to populate spinners with choices
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, planets);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        origin.setAdapter(adapter);
+        destination.setAdapter(adapter);
+
+        return calcView;
     }
 }
