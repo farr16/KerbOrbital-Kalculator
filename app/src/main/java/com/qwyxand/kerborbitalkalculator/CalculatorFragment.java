05332db14957855875df9c508ef5dbe1e500d7ea
Matthew Farr <msamfarr@outlook.com>
2016-09-06 17:06:21 -0700

    Add error checks for calculate button, and warning messages for if those errors are found. Adds text field for warning messages in calculator fragment layout.

--- app/src/main/java/com/qwyxand/kerborbitalkalculator/CalculatorFragment.java
+++ app/src/main/java/com/qwyxand/kerborbitalkalculator/CalculatorFragment.java
@@ -11,8 +11,6 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 /** CalculatorFragment
  * Created by Matthew on 9/2/2016.
  *
@@ -20,8 +18,6 @@ import org.w3c.dom.Text;
  * getting input from user input about the orbital transfer to be performed.
  */
 public class CalculatorFragment extends Fragment {
-
-
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View calcView = inflater.inflate(R.layout.fragment_calculator, container, false);
@@ -41,6 +37,7 @@ public class CalculatorFragment extends Fragment {
         final EditText parkingOrbitEntry = (EditText) calcView.findViewById(R.id.parkingOrbitEntry);
 
         // Obtain reference to labels we will be editing in code
+        final TextView warningMessageDisplay = (TextView) calcView.findViewById(R.id.warningMessageDisplay);
         final TextView phaseAngleDisplay = (TextView) calcView.findViewById(R.id.phaseAngleDisplay);
         final TextView ejectionAngleDisplay = (TextView) calcView.findViewById(R.id.ejectionAngleDisplay);
         final TextView ejectionVelocityDisplay= (TextView) calcView.findViewById(R.id.ejectionVelocityDisplay);
@@ -55,19 +52,36 @@ public class CalculatorFragment extends Fragment {
         // Set onClick behaviors for layout buttons
         calculateButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Test code. Tests if getting strings from spinners works properly, if getting
-                // the number from EditText works properly, and if setting text on TextViews works
-                // properly
-                phaseAngleDisplay.setText(origin.getSelectedItem().toString());
-                ejectionAngleDisplay.setText(destination.getSelectedItem().toString());
+                boolean flag = false;
+                String warnings = "";
+                int parkingOrbit = -1;
+
+                String orig = origin.getSelectedItem().toString();
+                String dest = destination.getSelectedItem().toString();
+
+                if (orig.equals(dest)) {
+                    warnings += getString(R.string.planet_selection_warning);
+                    flag = true;
+                }
+
                 try {
-                    int parkingOrbit = Integer.parseInt(parkingOrbitEntry.getText().toString());
-                    ejectionVelocityDisplay.setText("" + parkingOrbit);
+                    parkingOrbit = Integer.parseInt(parkingOrbitEntry.getText().toString());
                 }
                 catch (NumberFormatException e) {
-                    ejectionVelocityDisplay.setText("Enter an integer for parking orbit");
+                    warnings += getString(R.string.parking_orbit_input_warning);
+                    flag = true;
                 }
-                ejectionBurnDeltaVDisplay.setText("did it work?");
+
+                warningMessageDisplay.setText(warnings);
+
+                if (flag) {
+                    return;
+                }
+
+                System.out.println(orig);
+                System.out.println(dest);
+                System.out.println("Parking orbit: " + parkingOrbit);
+
             }
         });
         resetButton.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +90,8 @@ public class CalculatorFragment extends Fragment {
                 ejectionAngleDisplay.setText("");
                 ejectionVelocityDisplay.setText("");
                 ejectionBurnDeltaVDisplay.setText("");
+                parkingOrbitEntry.setText("");
+                warningMessageDisplay.setText("");
             }
         });
 
