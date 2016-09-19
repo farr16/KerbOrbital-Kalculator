c751ce714406026930764636eefcdb2112b44e20
Matthew Farr <msamfarr@outlook.com>
2016-09-18 22:27:42 -0700

    Overrides getItemPosition(object) and calls notifyDatasetChanged on adapter to update views.

--- app/src/main/java/com/qwyxand/kerborbitalkalculator/CustomFragmentPagerAdapter.java
+++ app/src/main/java/com/qwyxand/kerborbitalkalculator/CustomFragmentPagerAdapter.java
@@ -11,9 +11,21 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 public class CustomFragmentPagerAdapter extends FragmentPagerAdapter {
 
+    private final int PAGE_COUNT = 3;
+
+    private String origin;
+    private String destination;
+    private float phaseAngle;
+    private float ejectionAngle;
+
     // constructor
     public CustomFragmentPagerAdapter(FragmentManager fm){
         super(fm);
+        // Default values for initial view setup
+        origin = "Kerbin";
+        destination = "Duna";
+        phaseAngle = Float.NEGATIVE_INFINITY;
+        ejectionAngle = Float.NEGATIVE_INFINITY;
     }
 
     @Override
@@ -21,22 +33,46 @@ public class CustomFragmentPagerAdapter extends FragmentPagerAdapter {
      * Given a position index, returns the fragment corresponding to that index.
      */
     public Fragment getItem(int pos) {
-        if (pos == 0)
+        if (pos == 0) {
             return new CalculatorFragment();
-        else if (pos == 1)
-            return new PhaseAngleDisplayFragment();
-        else if (pos == 2)
-            return new EjectionAngleDisplayFragment();
+        }
+        else if (pos == 1) {
+            return PhaseAngleDisplayFragment.newInstance(origin, destination, phaseAngle);
+        }
+        else if (pos == 2) {
+            return EjectionAngleDisplayFragment.newInstance(origin, ejectionAngle);
+        }
         else
             return null;
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        if (object instanceof CalculatorFragment)
+            ;
+        else if (object instanceof PhaseAngleDisplayFragment) {
+            ((PhaseAngleDisplayFragment) object).updatePhaseDisplay(origin, destination, phaseAngle);
+        }
+        else if (object instanceof EjectionAngleDisplayFragment) {
+            ((EjectionAngleDisplayFragment) object).updateEjectionDisplay(origin, ejectionAngle);
+        }
+        return super.getItemPosition(object);
     }
 
     @Override
     /** getCount
      * Returns the number of tabs in the application interface, currently fixed at three.
      */
-    public int getCount() {
-        return 3;
-    }
+    public int getCount() {return PAGE_COUNT;}
+
+    /* Setter methods for the viewpager dataset variables
+    * */
+    public void setOrigin (String orig) {origin = orig;}
+
+    public void setDestination (String dest) {destination = dest;}
+
+    public void setPhaseAngle (float phase) {phaseAngle = phase;}
+
+    public void setEjectionAngle (float eject) {ejectionAngle = eject;}
 
 }
